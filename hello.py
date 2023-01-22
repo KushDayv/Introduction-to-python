89aadf201cfb1608dc6e244ee7bf948f3bd3d598
SammyK <johnsamuelthiongo52@gmail.com>
2023-01-23 00:59:44 +0300

    first commit

---
+++ hello.py
@@ -0,0 +1,25 @@
+#INTRODUCTION TO PYTHON
+"""The codes written below will help you to get the basic knowledge of coding
+in python . Python is an interpreted language as compared to other languages which 
+are compiled. This means that it does not need to be compiled to machine language for it to 
+run....WELCOME"""
+print("My first code in python")
+
+#different datatypes
+a = 'John Samuel' #string (str)
+
+print("My name is " + a )
+b = age = 21 #integer(int)
+c = wight = 60.5 #float 
+y = True #boolean
+
+z = {"apple","banana","cherry"}#set
+print(b)
+print(y)
+print(c)
+print(z)
+
+print(complex(b))#type of number
+
+
+
