d2a516189962ee1d492679bb7e70d76e148bf3ea
sammyk <johnsamuelthiongo52@gmail.com>
2023-02-14 16:28:01 +0300

    a simple python game

---
+++ python_game.py
@@ -0,0 +1,13 @@
+import numpy
+
+from numpy import random
+
+winning_number = random.randint(0,50)
+num = int(input("Guess any number :"))
+if num == winning_number:
+    print("YOU WIN!!!")
+elif num < winning_number:
+    print("The value is too low")    
+elif num > winning_number:
+    print("The value is too high")
+        
