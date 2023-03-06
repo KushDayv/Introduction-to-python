fa890c419d2ff4fa7d1437eaa5acf05a7a8985dc
sammyk <johnsamuelthiongo52@gmail.com>
2023-03-06 15:55:30 +0300

    python inheritance

---
+++ Inheritance/multiple.py
@@ -0,0 +1,31 @@
+#A program to show multiple inheritance in python
+
+name = input("Enter the name of the owner institution of the car : ")
+model = input("Enter the model of the car : ")
+color = input("Enter the color of the car : ")
+engine = input("Enter the engine size of the vehicle : ")
+
+
+
+
+
+class Car:
+    def __init__(self,model,color,engine):
+        self.model = model
+        self.color = color
+        self.engine = engine
+        self.name = name
+ 
+class Owner:
+    def person(self):
+        return name
+
+
+
+class Institution(Car,Owner):
+    def zetech(self):
+        return (model,color,engine)
+    
+F = Institution()
+print("The Car Details are : ",F.zetech())   
+print("The car belongs to : ",F.person())
---
+++ Inheritance/single.py
@@ -0,0 +1,30 @@
+"""
+///use of the base, super class, parent class- this is the class from which another class takes 
+attributes from
+///the child, sub class, derived 
+
+Data abstraction - data hiding
+Encapsulation - wrapping of data
+Polymophism - taking many forms
+"""
+#A program to show single inheritance in python
+
+class Animal:
+    def legs(self):
+        print("I have four legs")
+
+    def fur(self):
+        print("My body is covered with fur")
+
+class Dog(Animal):  #class Dog inherits from the class animal
+    def bark(self):
+        print("The dog barks")
+
+
+
+
+d = Dog()
+d.legs()
+d.fur()
+d.bark()            
+
