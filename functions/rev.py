704279a67c9bb5dcc7bfc02b8538c84bb6582068
sammyk <johnsamuelthiongo52@gmail.com>
2023-04-13 21:15:37 +0300

    updated

---
+++ functions/rev.py
@@ -0,0 +1,30 @@
+# words = ['apple', 'banana', 'orange', 'grape']
+
+# doubled_words = []
+
+# for word in words:
+#     doubled_words.append(word)
+#     doubled_words.append(word)
+
+# print(doubled_words)
+
+
+
+quantity = int(input("Enter a quantity: "))  
+
+total = quantity * 100
+
+if total > 1000:
+    discount = 0.1 * total
+    total_cost = total - discount
+
+    print("Your discount is: ", discount)
+    print("Your total price that you will pay is: ", total_cost)
+
+else:
+    print("No discount, You will pay: ",total)   
+
+
+"""In this program the quantity is taken as the number of items that the customer is buying
+based on the fact that each item costs 100,,,,,so you multiply the quantity entered and then multiply
+with 100 and from there calculate whether the customer will get discount or not """
